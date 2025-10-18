654220f876fafa2b9864da067a2d6ce6e1a7e51f
Your Name <you@example.com>
2025-10-18 18:14:40 +0100

    Updated

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
-# Register your models here.
+# If you have models in payments.models, register them with an icon column.
+# Example placeholder admin for any future Payment model:
+
+
+# from .models import Payment
+
+
+class PaymentAdminPlaceholder(admin.ModelAdmin):
+	list_display = ("icon",)
+
+	def icon(self, obj):
+		return format_html("<i class='fas fa-credit-card' style='font-size:16px;color:#0D1B52;'></i>")
+	icon.short_description = ''
+
+# If you add a Payment model later, register it like this:
+# admin.site.register(Payment, PaymentAdminPlaceholder)
--- utils/admin.py
+++ utils/admin.py
@@ -1,8 +1,46 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import FAQ, TeamMember, Career, ContactMessage
 
 
-admin.site.register(FAQ)
-admin.site.register(TeamMember)
-admin.site.register(Career)
-admin.site.register(ContactMessage)
+@admin.register(FAQ)
+class FAQAdmin(admin.ModelAdmin):
+	list_display = ("icon", "question", "created_at") if hasattr(FAQ, 'question') else ("icon",)
+
+	def icon(self, obj):
+		return format_html("<i class='fas fa-question-circle' style='font-size:14px;color:#0D1B52;'></i>")
+	icon.short_description = ''
+
+
+@admin.register(TeamMember)
+class TeamMemberAdmin(admin.ModelAdmin):
+	list_display = ("photo_preview", "name", "role") if hasattr(TeamMember, 'name') else ("photo_preview",)
+
+	def photo_preview(self, obj):
+		img = getattr(obj, 'photo', None) or getattr(obj, 'avatar', None)
+		if img:
+			try:
+				url = img.url if hasattr(img, 'url') else img
+				return format_html("<img src='{}' style='max-height:48px; object-fit:contain; border-radius:6px;'/>", url)
+			except Exception:
+				pass
+		return format_html("<i class='fas fa-user-tie' style='font-size:18px;color:#0D1B52;'></i>")
+	photo_preview.short_description = ''
+
+
+@admin.register(Career)
+class CareerAdmin(admin.ModelAdmin):
+	list_display = ("icon", "title") if hasattr(Career, 'title') else ("icon",)
+
+	def icon(self, obj):
+		return format_html("<i class='fas fa-briefcase' style='font-size:14px;color:#0D1B52;'></i>")
+	icon.short_description = ''
+
+
+@admin.register(ContactMessage)
+class ContactMessageAdmin(admin.ModelAdmin):
+	list_display = ("icon", "subject", "email") if hasattr(ContactMessage, 'subject') else ("icon",)
+
+	def icon(self, obj):
+		return format_html("<i class='fas fa-envelope' style='font-size:14px;color:#0D1B52;'></i>")
+	icon.short_description = ''
